f846123d2c49d7592415c0a03cc0276cca214e60
DhaiAssaf <dhasaf@icloud.com>
2024-04-07 01:15:31 +0300

    Initial Commit

---
+++ ToDoListApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListApplication
+//
+//  Created by Dhai Alassaf on 28/09/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListApplication/ToDoListApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApplicationApp.swift
+//  ToDoListApplication
+//
+//  Created by Dhai Alassaf on 28/09/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
