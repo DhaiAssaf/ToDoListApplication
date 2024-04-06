4e58218f976e9e7e29a49c9d1149b2e8cce32520
DhaiAssaf <dhasaf@icloud.com>
2024-04-07 01:51:11 +0300

    Create views
    - Create needed views: Add, edit, details
    - Create home page
    - Create list body

---
+++ ToDoListApplication/EditTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTaskView.swift
+//  ToDoListApplication
+//
+//  Created by Dhai Alassaf on 28/09/1445 AH.
+//
+
+import SwiftUI
+
+struct EditTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTaskView()
+}
---
+++ ToDoListApplication/TaskDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDetailsView.swift
+//  ToDoListApplication
+//
+//  Created by Dhai Alassaf on 28/09/1445 AH.
+//
+
+import SwiftUI
+
+struct TaskDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDetailsView()
+}
